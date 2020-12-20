96ec1246a35f127f5d7e31af2cb0f8a245ff152a
bbubbush <bbubbush@gmail.com>
2020-12-21 01:53:28 +0900

    add stream sample

--- java8/src/com/tistory/bbubbush/defaultfi/AppRunner.java
+++ java8/src/com/tistory/bbubbush/defaultfi/AppRunner.java
@@ -1,7 +1,6 @@
 package com.tistory.bbubbush.defaultfi;
 
-import java.util.function.BiFunction;
-import java.util.function.Function;
+import java.util.function.*;
 
 public class AppRunner {
     public static void main(String[] args) {
@@ -25,5 +24,27 @@ public class AppRunner {
         BiFunction<Integer, Integer, Integer> biFunction2 = (param1, param2) -> Integer.max(param1, param2);
         System.out.println(biFunction2.apply(10, 20));
         System.out.println(biFunction.andThen(function2).apply(10, 20));  // andThen을 통해 체이닝도 가능하다.
+
+        System.out.println("\n=== Consumer ===");   // 하나의 파라미터를 정의하고 리턴타입이 없다.
+        Consumer<String> consumer = (str) -> System.out.println(str.toUpperCase());
+        consumer.accept("bbubbush");
+
+        System.out.println("\n=== Supplier ===");   // 파라미터 없이 리턴타입만 정의한다.
+        Supplier<Integer> supplier = () -> Integer.MAX_VALUE;
+        System.out.println(supplier.get());
+
+        System.out.println("\n=== Predicate ===");   // 하나의 파라티머 타입을 정의하여 boolean 값을 리턴한다.
+        Predicate<String> predicate = (s -> s.contains("b"));
+        System.out.println(predicate.test("bbubbush"));
+        System.out.println(predicate.negate().test("bbubbush"));
+        System.out.println(predicate.or((str) -> str.contains("a")).test("ccuccush"));
+
+        System.out.println("\n=== UnaryOperator ===");   // Function의 확장된 함수로 하나의 파라미터와 리턴타입이 같은 경우 사용한다.
+        UnaryOperator<Integer> unaryOperator = (i) -> Integer.sum(i, 10);
+        System.out.println(unaryOperator.apply(100));
+
+        System.out.println("\n=== UnaryOperator ===");   // BiFunction의 확장된 함수로 두 개의 파라미터와 리턴타입이 같은 경우 사용한다.
+        BinaryOperator<Integer> binaryOperator = Integer::sum;
+        System.out.println(binaryOperator.apply(100, 200));
     }
 }
