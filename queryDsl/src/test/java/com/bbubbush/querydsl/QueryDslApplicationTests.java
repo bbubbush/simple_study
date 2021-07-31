9745d3bf2959bd5086176480babf29065ef5d452
bbubbush <bbubbush@gmail.com>
2021-07-31 14:48:54 +0900

    QueryDsl 시작

---
+++ queryDsl/src/test/java/com/bbubbush/querydsl/QueryDslApplicationTests.java
@@ -0,0 +1,38 @@
+package com.bbubbush.querydsl;
+
+import com.bbubbush.querydsl.entity.Hello;
+import com.bbubbush.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QueryDslApplicationTests {
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+
+}
